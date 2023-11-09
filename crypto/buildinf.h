c8a42c1d6e25722adfa03f47f08b82733720512a
Arne Schwabe <arne@rfc2549.org>
2023-11-09 17:33:33 +0100

    Import OpenSSL 3.1.4

--- crypto/buildinf.h
+++ crypto/buildinf.h
@@ -11,7 +11,7 @@
  */
 
 #define PLATFORM "platform: linux-armv4"
-#define DATE "built on: Wed Oct 11 11:47:56 2023 UTC"
+#define DATE "built on: Thu Nov  9 15:26:10 2023 UTC"
 
 /*
  * Generate compiler_flags as an array of individual characters. This is a
